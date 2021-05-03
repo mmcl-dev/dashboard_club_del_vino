b3a2737d34de81410af91a938959147a1e0692d8
NearLawliet <gon.near@gmail.com>
2021-05-03 20:19:58 -0300

    agregado de imagen de carga, terminado mostrar ultimo producto

--- src/components/ContentRowTop.js
+++ src/components/ContentRowTop.js
@@ -6,19 +6,18 @@ import InfoProduct from './InfoProduct';
 
 function ContentRowTop () {
     return (
-				<div className="container-fluid">
-					<div className="d-sm-flex align-items-center justify-content-between mb-4">
-						<h1 className="h3 mb-0 text-gray-800">Dashboard: Club del vino</h1>
-					</div>
-				
-					<ContentRowProducts />					
-	
-					<div className="row">
-						<InfoProduct />
-                        <CategoriesInDB />
-					</div>
-				</div>
-				
+		<div className="container-fluid">
+			<div className="d-sm-flex align-items-center justify-content-between mb-4">
+				<h1 className="h3 mb-0 text-gray-800">Dashboard: Club del vino</h1>
+			</div>
+		
+			<ContentRowProducts />					
+
+			<div className="row">
+				<InfoProduct />
+				<CategoriesInDB />
+			</div>
+		</div>				
     )
 }
 
